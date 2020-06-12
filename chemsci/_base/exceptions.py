1905c0b18a3b96d35f67c308eac179cf884931a1
Bundaberg-Joey <crh53@bath.ac.uk>
2020-06-12 15:39:13 +0100

    Updated `produce_list` method to work with numpy arrays.

---
+++ chemsci/_base/exceptions.py
@@ -0,0 +1,5 @@
+class BitVectorRepresentationError(Exception):
+    """Named Exception raised when unable to convert the numpy array representation of molecular fingerprint
+    to a string.
+    """
+    pass
