32fa168ef5dd2b91c1838dd2fb343fd56d6fa4d3
Bundaberg-Joey <crh53@bath.ac.uk>
2020-08-10 22:42:32 +0100

    Added `StandardRepresentationError` to named exceptions.

--- chemsci/base/exceptions.py
+++ chemsci/base/exceptions.py
@@ -1,3 +1,8 @@
+class StandardRepresentationError(Exception):
+    """Named Exception raised when invalid representation selection passed to `StandardFeatureTransformer` constructor
+    method.
+    """
+
 class BitVectorRepresentationError(Exception):
     """Named Exception raised when unable to convert the numpy array representation of molecular fingerprint
     to a string.
