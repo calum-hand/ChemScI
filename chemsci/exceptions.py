4d80911e029e963b6a1da8ba69ef3a33321d190a
Bundaberg-Joey <crh53@bath.ac.uk>
2021-02-01 20:31:09 +0000

    Updating and cleaning out exceptions.py file of no longer used exceptions.

--- chemsci/exceptions.py
+++ chemsci/exceptions.py
@@ -1,29 +1,45 @@
-class StandardRepresentationError(Exception):
-    """Named Exception raised when invalid representation selection passed to `StandardFeatureTransformer` constructor
-    method.
-    """
-
 class BitVectorRepresentationError(Exception):
-    """Named Exception raised when unable to convert the numpy array representation of molecular fingerprint
-    to a string.
+    """Unable to convert the numpy array representation of molecular fingerprint to a string.
     """
     pass
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 class FingerprintRepresentationError(Exception):
-    """Named Exception raised when fingerprints are unable to be interacted with due to incorrect types or mismatched
-    numpy array sizes.
+    """Features are unable to be interacted with due to incorrect types or mismatched numpy array sizes.
     """
     pass
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 class JsonSerialisationError(Exception):
-    """Named Exception raised if FingerprintFactory is unable to correctly serialise data into JSON format.
+    """FeatureFactory is unable to correctly serialise data into JSON format.
     """
     pass
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 class UserSelectionError(Exception):
-    """Named Exception for when an invalid selection is made by the user.
+    """Invalid selection is made by the user, typically when initialising objects with default args.
     """
     pass
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class ConversionError(Exception):
+    """Unable to convert the passed representation into a workable object for featurisation.
+    """
+    pass
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class FeaturisationError(Exception):
+    """Unable to featureise the passed converted representation."""
+    pass
+
+# ----------------------------------------------------------------------------------------------------------------------
