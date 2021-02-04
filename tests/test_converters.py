d1270d92165a723c393938b7f366a89375d94e00
Bundaberg-Joey <crh53@bath.ac.uk>
2021-02-04 23:22:03 +0000

    Added drafts for tests to implement.

---
+++ tests/test_converters.py
@@ -0,0 +1,2 @@
+# All default dict entries (will need files for mol and pdb)
+# pubchem, assert can get result and convert to puchem object (dont care about sleep)
---
+++ tests/test_featurisers.py
@@ -0,0 +1,4 @@
+# for the same molecule, ensure each main fingerprint type is generated succesfully (store in file)
+# DO basic types of morgan fingerprints
+# pubchem fingerprints, generate for same molecule (CID number vs SMILES)
+# MAP4 same for variations
