545bbea0d939dcf81dfc8ee0650a8aa7f7400a8e
Bundaberg-Joey <crh53@bath.ac.uk>
2021-02-04 23:22:03 +0000

    Added drafts for tests to implement.

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -2,3 +2,10 @@ import pytest
 
 def test_installation():
     assert True
+
+# check init works fine
+# `convert_rep` / `featurise_mol` return as expected / raise correct errors
+# `transform`, for given X returns expected array of fingerprints (also attributes are set)
+# to<blank>
+# to_<file> --> check file and read back to make sure all makes sense
+# `__len__` returns correct number (just set arbitrary list size)
