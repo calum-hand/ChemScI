fa8a1e2aba9a338703088e0b9bfdcd96a40a1d28
Bundaberg-Joey <crh53@bath.ac.uk>
2021-02-04 21:59:58 +0000

    Adding setup files for the conda build.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ChemScI',
+    version='0.1.0',
+    description='Flexible API for working with molecular representations in machine learning and informatics.',
+    author='Calum Hand',
+    url='https://github.com/Bundaberg-Joey/ChemScI',
+)
