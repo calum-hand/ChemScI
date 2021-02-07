05697b7183a3f4708620ac2b1de989373da92a52
Bundaberg-Joey <crh53@bath.ac.uk>
2021-02-07 19:46:07 +0000

    updating constants for feature tests.

--- tests/constants.py
+++ tests/constants.py
@@ -1,5 +1,29 @@
+import pickle
 from rdkit.Chem import MolFromSmiles
+from pubchempy import Compound
 
-SMILES = 'O=C1C(=Cc2ccccc2)CCCC1=Cc1ccccc1'
-MOL = MolFromSmiles(SMILES)
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+with open('tests/files/features.pkl', 'rb') as f:
+    REF_FEATURES = pickle.load(f)
+
+    STD_FEATURES = REF_FEATURES['standard']
+    MAP4_FEATURES = REF_FEATURES['map4']
+    PUB_FEATURES = REF_FEATURES['pubchem']
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+smiles = 'O=C1C(=Cc2ccccc2)CCCC1=Cc1ccccc1'
+MOL = MolFromSmiles(smiles)
 assert MOL, 'Error creating the testing mol object.'
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+cid = 5090
+PUB_MOL = Compound.from_cid(cid)
+assert isinstance(PUB_MOL, Compound), 'Error creating the testing pubchem Compound object.'
+
+# ----------------------------------------------------------------------------------------------------------------------
